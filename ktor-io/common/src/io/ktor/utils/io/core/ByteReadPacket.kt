bc3f984a20999c6c145e16e06127dadfb05f63c3
leonid.stashevsky <leonid.stashevsky@gmail.com>
2023-10-05 14:50:34 +0200

    KTOR-6030 Cleanup byte channel API

--- ktor-io/common/src/io/ktor/utils/io/core/ByteReadPacket.kt
+++ ktor-io/common/src/io/ktor/utils/io/core/ByteReadPacket.kt
@@ -43,6 +43,21 @@ public class ByteReadPacket internal constructor(
         return "ByteReadPacket[${hashCode().toHexString()}]"
     }
 
+    public fun readPacket(size: Int): ByteReadPacket {
+        require(size >= 0) { "Size shouldn't be negative: $size" }
+        require(remaining >= size) { "Not enough bytes available ($remaining) to read $size bytes" }
+
+        if (size == remaining.toInt()) {
+            val result = copy()
+            release()
+            return result
+        }
+
+        return buildPacket {
+            writePacket(this@ByteReadPacket, size)
+        }
+    }
+
     public companion object {
         public val Empty: ByteReadPacket = ByteReadPacket(ChunkBuffer.Empty, 0L, ChunkBuffer.EmptyPool)
     }
